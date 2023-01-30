0e4e09405e47aa5d5b79cafdab21cccb75cf9909
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-01-30 15:30:21 +0000

    [2015] Solution for 01 day

---
+++ aoc-2015/src/bin/day01.rs
@@ -0,0 +1,36 @@
+#![feature(control_flow_enum)]
+
+use std::ops::ControlFlow;
+
+use aoc_2015::*;
+
+const DAY: i32 = 1;
+type Solution = i32;
+
+fn main() {
+    let input = get_input_text(DAY);
+
+    let steps = input.chars().map(|ch| match ch {
+        '(' => 1,
+        ')' => -1,
+        _ => 0,
+    });
+
+    let solution1: Solution = steps.clone().sum();
+    let solution2: Solution = steps
+        .clone()
+        .enumerate()
+        .try_fold(0, |level, (i, step)| {
+            let new_level = level + step;
+            if new_level < 0 {
+                ControlFlow::Break(i + 1)
+            } else {
+                ControlFlow::Continue(new_level)
+            }
+        })
+        .break_value()
+        .unwrap() as Solution;
+
+    show_solution(DAY, solution1);
+    show_solution(DAY, solution2);
+}
