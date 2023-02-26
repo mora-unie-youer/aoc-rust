1d78e6800953aac483efaaf91fb6fac82e881941
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-02-26 19:02:06 +0000

    [2016] Improved solution for day 03

--- aoc-2016/src/bin/day03.rs
+++ aoc-2016/src/bin/day03.rs
@@ -1,54 +1,37 @@
-#![feature(iter_array_chunks)]
-#![feature(once_cell)]
+#![feature(array_chunks)]
 
 use aoc_2016::*;
 
 const DAY: i32 = 3;
 type Solution = usize;
 
+fn check_triangle(mut sides: [usize; 3]) -> bool {
+    sides.sort();
+    sides[0] + sides[1] > sides[2]
+}
+
 fn main() {
     let input = get_input_text(DAY);
-    let regex = regex!(r"\s*(\d+)\s*(\d+)\s*(\d+)");
-
-    let solution1: Solution = input
+    let sides: Vec<Vec<usize>> = input
         .lines()
-        .filter(|line| {
-            let mut result: Vec<_> = regex
-                .captures(line)
-                .unwrap()
-                .iter()
-                .skip(1)
-                .map(|capture| capture.unwrap().as_str().parse::<usize>().unwrap())
-                .collect();
-            result.sort();
-            result[0] + result[1] > result[2]
+        .map(|line| {
+            line.split_whitespace()
+                .map(|v| v.parse().unwrap())
+                .collect()
         })
+        .collect();
+
+    let solution1: Solution = sides
+        .iter()
+        .filter(|&s| check_triangle([s[0], s[1], s[2]]))
         .count();
 
-    let solution2: Solution = input
-        .lines()
+    let solution2: Solution = sides
         .array_chunks()
-        .map(|lines: [&str; 3]| {
-            let captures: Vec<Vec<usize>> = lines
-                .iter()
-                .map(|line| regex.captures(line).unwrap())
-                .map(|captures| {
-                    captures
-                        .iter()
-                        .skip(1)
-                        .map(|capture| capture.unwrap().as_str().parse().unwrap())
-                        .collect()
-                })
-                .collect();
-
-            captures[0]
-                .iter()
-                .zip(captures[1].iter().zip(captures[2].iter()))
-                .filter(|&(&a, (&b, &c))| {
-                    let mut sides: [usize; 3] = [a, b, c];
-                    sides.sort();
-                    sides[0] + sides[1] > sides[2]
-                })
+        .map(|[s1, s2, s3]| {
+            s1.iter()
+                .zip(s2.iter().zip(s3.iter()))
+                .filter(|&(&a, (&b, &c))| check_triangle([a, b, c]))
                 .count()
         })
         .sum();
