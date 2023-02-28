b2617f8487a4eafcb9759371143fb24b6801b8d7
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-02-28 03:58:25 +0000

    [2016] Improved solution for day 05

--- aoc-2016/src/bin/day05.rs
+++ aoc-2016/src/bin/day05.rs
@@ -1,5 +1,5 @@
 use aoc_2016::*;
-use crypto::{md5::Md5, digest::Digest};
+use crypto::{digest::Digest, md5::Md5};
 
 const DAY: i32 = 5;
 type Solution = String;
@@ -8,53 +8,38 @@ fn main() {
     let input = get_input_text(DAY);
     let door_id = input.trim();
 
-    let solution1: Solution = {
-        let mut n = 0;
-        let mut password: String = String::new();
-        let mut digest = Md5::new();
+    let mut password1 = String::new();
+    let mut password2 = ['-'; 8];
 
-        while password.len() < 8 {
-            digest.input_str(door_id);
-            digest.input_str(&n.to_string());
-            let result = digest.result_str();
-            digest.reset();
+    let mut n = 0;
+    let mut found = 0;
+    let mut digest = Md5::new();
 
-            if result.starts_with("00000") {
-                password.push(result.chars().nth(5).unwrap());
+    // password.len() < 8 && found < 8 -> found < 8 will always be later
+    while found < 8 {
+        digest.input_str(door_id);
+        digest.input_str(&n.to_string());
+        let result = digest.result_str();
+        digest.reset();
+
+        if result.starts_with("00000") {
+            if password1.len() < 8 {
+                password1.push(result.chars().nth(5).unwrap());
             }
 
-            n += 1;
-        }
-
-        password
-    };
-
-    let solution2: Solution = {
-        let mut n = 0;
-        let mut found = 0;
-        let mut password = ['-'; 8];
-        let mut digest = Md5::new();
-
-        while found < 8 {
-            digest.input_str(door_id);
-            digest.input_str(&n.to_string());
-            let result = digest.result_str();
-            digest.reset();
-
-            if result.starts_with("00000") {
-                let position = result.chars().nth(5).iter().collect::<String>();
-                let position = usize::from_str_radix(&position, 16).unwrap();
-                if position < 8 && password[position] == '-' {
-                    password[position] = result.chars().nth(6).unwrap();
-                    found += 1;
-                }
+            let position = result.chars().nth(5).iter().collect::<String>();
+            let position = usize::from_str_radix(&position, 16).unwrap();
+            if position < 8 && password2[position] == '-' {
+                password2[position] = result.chars().nth(6).unwrap();
+                found += 1;
             }
-
-            n += 1;
         }
 
-        password.iter().collect()
-    };
+        n += 1;
+    }
+
+    let solution1: Solution = password1;
+    let solution2: Solution = password2.iter().collect();
 
     show_solution(DAY, solution1);
     show_solution(DAY, solution2);
