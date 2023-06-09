74e5aefda4995b25dcde155d8a0e8261f70851a3
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-06-09 15:22:07 +0000

    [2020] Improved solution for day 05

--- aoc-2020/src/bin/day05.rs
+++ aoc-2020/src/bin/day05.rs
@@ -4,32 +4,9 @@ const DAY: i32 = 5;
 type Solution = usize;
 
 fn get_seat_id(seat: &str) -> usize {
-    let (row_seat, col_seat) = (&seat[0..7], &seat[7..]);
-
-    let (mut row_low, mut row_high) = (0, 127);
-    let (mut col_low, mut col_high) = (0, 7);
-
-    for step in row_seat.chars() {
-        let dy = (row_high - row_low) / 2;
-
-        match step {
-            'F' => row_high = row_low + dy,
-            'B' => row_low += dy + 1,
-            _ => unreachable!(),
-        }
-    }
-
-    for step in col_seat.chars() {
-        let dx = (col_high - col_low) / 2;
-
-        match step {
-            'L' => col_high = col_low + dx,
-            'R' => col_low += dx + 1,
-            _ => unreachable!(),
-        }
-    }
-
-    row_low * 8 + col_low
+    seat.chars()
+        .map(|ch| matches!(ch, 'B' | 'R'))
+        .fold(0, |acc, v| (acc << 1) | v as usize)
 }
 
 fn main() {
