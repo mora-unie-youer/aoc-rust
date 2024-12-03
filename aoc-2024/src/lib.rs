96cb63e552feed65ab7f1fa5a95d73b538c0a93e
Mora Unie Youer <mora_unie_youer@riseup.net>
2024-12-03 16:05:57 +0300

    [2024] Add `regex`

--- aoc-2024/src/lib.rs
+++ aoc-2024/src/lib.rs
@@ -7,3 +7,11 @@ pub const YEAR: i32 = 2024;
 pub fn get_input_text(day: i32) -> Cow<'static, str> {
     aoc::get_input_text(YEAR, day)
 }
+
+#[macro_export]
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: std::sync::OnceLock<regex::Regex> = std::sync::OnceLock::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
